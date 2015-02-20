b7b91691cd9c58ddfc1652e340cd81c5eda29736
Samuel De Backer <sdebacker@gmail.com>
2015-02-20 01:31:39 +0100

    licence + sidebar

--- src/Composers/SideBarViewComposer.php
+++ src/Composers/SideBarViewComposer.php
@@ -1,18 +1,25 @@
 <?php
 namespace TypiCMS\Modules\Files\Composers;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
+use TypiCMS\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->menus['media']->put('files', [
-            'weight' => config('typicms.files.sidebar.weight'),
-            'request' => $view->prefix . '/files*',
-            'route' => 'admin.files.index',
-            'icon-class' => 'icon fa fa-fw fa-file-photo-o',
-            'title' => 'Files',
-        ]);
+        $view->sidebar->group(trans('global.menus.media'), function (SidebarGroup $group) {
+            $group->addItem(trans('files::global.name'), function (SidebarItem $item) {
+                $item->icon = config('typicms.files.sidebar.icon', 'icon fa fa-fw fa-file-photo-o');
+                $item->weight = config('typicms.files.sidebar.weight');
+                $item->route('admin.files.index');
+                $item->append('admin.files.create');
+                $item->authorize(
+                    $this->auth->hasAccess('files.index')
+                );
+            });
+        });
     }
 }
