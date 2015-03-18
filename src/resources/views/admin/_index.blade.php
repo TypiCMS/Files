4af5265477d59906b5a9045dedc05c535a96fdf7
Samuel De Backer <sdebacker@gmail.com>
2015-03-18 23:11:18 +0100

    _index.blade

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a id="uploaderAddButtonContainer" href="#"><i id="uploaderAddButton" class="fa fa-plus-circle"></i><span class="sr-only">{{ ucfirst(trans('files::global.New')) }}</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} files">{{ models.length }} file</span>
+        <a id="uploaderAddButtonContainer" href="#"><i id="uploaderAddButton" class="fa fa-plus-circle"></i><span class="sr-only">@{{ ucfirst(trans('files::global.New')) }}</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} files">@{{ models.length }} file</span>
     </h1>
 
     <div class="dropzone" drop-zone="" id="dropzone">
@@ -29,10 +29,10 @@
                 <tr>
                     <td colspan="5"></td>
                     <td>
-                        <input st-search="file" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="file" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                     <td>
-                        <input st-search="alt_attribute" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="alt_attribute" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                     <td></td>
                     <td></td>
@@ -43,18 +43,18 @@
                 <tr ng-repeat="model in displayedModels">
                     <td><typi-btn-delete ng-click="delete(model, model.file)"></typi-btn-delete></td>
                     <td typi-btn-edit></td>
-                    <td>{{ model.created_at | dateFromMySQL:'short' }}</td>
-                    <td>{{ model.type }}</td>
+                    <td>@{{ model.created_at | dateFromMySQL:'short' }}</td>
+                    <td>@{{ model.type }}</td>
                     <td ng-switch="model.type">
-                        <img ng-switch-when="i" ng-src="{{ model.thumb_src }}" alt="{{ model.alt_attribute }}">
+                        <img ng-switch-when="i" ng-src="@{{ model.thumb_src }}" alt="@{{ model.alt_attribute }}">
                         <span class="fa fa-fw fa-file-o" ng-switch-default></span>
                     </td>
-                    <td>{{ model.file }}</td>
+                    <td>@{{ model.file }}</td>
                     <td contentEditable highlighter="model.alt_attribute" ng-model="model.alt_attribute" ng-blur="update(model)">
-                        {{ model.alt_attribute }}
+                        @{{ model.alt_attribute }}
                     </td>
-                    <td>{{ model.width }}</td>
-                    <td>{{ model.height }}</td>
+                    <td>@{{ model.width }}</td>
+                    <td>@{{ model.height }}</td>
                 </tr>
             </tbody>
             <tfoot>
--- src/resources/views/admin/filepicker.blade.php
+++ src/resources/views/admin/filepicker.blade.php
@@ -21,6 +21,25 @@ col-xs-12
 
 @section('main')
 
-    @include('files::admin.thumbnails')
+<div ng-app="typicms" ng-cloak ng-controller="ListController">
+
+    <h1>
+        <a id="uploaderAddButtonContainer" href="#"><i id="uploaderAddButton" class="fa fa-plus-circle"></i><span class="sr-only">@{{ ucfirst(trans('files::global.New')) }}</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} files">@{{ models.length }} file</span>
+    </h1>
+
+    <div class="dropzone hidden" drop-zone="" id="dropzone">
+        <div class="dz-message" translate>Click or drop files to upload.</div>
+    </div>
+
+    <div class="thumbnail" ng-repeat="model in models" id="item_@{{ model.id }}">
+        <img class="img-responsive" ng-src="@{{ model.thumb_sm }}" alt="">
+        <div class="caption">
+            <a href="#" class="btn btn-default btn-xs btn-block btn-insert" ng-click="selectAndClose(<?php echo Input::get('CKEditorFuncNum') ?>, '/' + model.path + '/' + model.file)" translate>Insert</a>
+            <small>@{{ model.file }}</small>
+        </div>
+    </div>
+
+</div>
 
 @stop
