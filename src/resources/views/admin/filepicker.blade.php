7b6ab56f0a6bd9b0317c1ee5d1f390d6e9c28bb5
Samuel De Backer <sdebacker@gmail.com>
2015-05-26 22:22:49 +0200

    filepicker

--- src/resources/views/admin/filepicker.blade.php
+++ src/resources/views/admin/filepicker.blade.php
@@ -11,17 +11,12 @@ col-xs-12
 @stop
 @section('breadcrumbs')
 @stop
-
-@section('h1')
-    <span id="nb_elements">{{ $models->total() }}</span> @choice('files::global.files', $models->total())
-@stop
-
 @section('titleLeftButton')
 @stop
 
 @section('main')
 
-<div ng-app="typicms" ng-cloak ng-controller="ListController">
+<div class="container-filepicker" ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
         <a id="uploaderAddButtonContainer" href="#"><i id="uploaderAddButton" class="fa fa-plus-circle"></i><span class="sr-only">@{{ ucfirst(trans('files::global.New')) }}</span></a>
@@ -34,16 +29,15 @@ col-xs-12
 
     <div class="row">
         <div class="thumbnail" ng-repeat="model in models" id="item_@{{ model.id }}">
-            <div ng-switch="model.type">
+            <div class="btn-insert" ng-switch="model.type" ng-click="selectAndClose(<?php echo Input::get('CKEditorFuncNum') ?>, '/' + model.path + '/' + model.file)">
                 <img class="img-responsive" ng-switch-when="i" ng-src="@{{ model.thumb_sm }}" alt="@{{ model.alt_attribute }}">
                 <span class="doc fa fa-fw fa-file-o" ng-switch-default></span>
             </div>
-            <a href="#" class="btn btn-default btn-xs btn-block btn-insert" ng-click="selectAndClose(<?php echo Input::get('CKEditorFuncNum') ?>, '/' + model.path + '/' + model.file)" translate>Insert</a>
         </div>
     </div>
 
-</div>
-
     {!! $models->appends(Input::except('page'))->render() !!}
+
+</div>
 
 @stop
