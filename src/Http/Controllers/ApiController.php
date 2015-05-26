89b468a76a9536a1f09cd0b4c1fc4c8a4e155ac4
Samuel De Backer <sdebacker@gmail.com>
2015-05-26 21:59:55 +0200

    …

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -19,8 +19,15 @@ class ApiController extends BaseApiController
      */
     public function index()
     {
+        $gallery_id = Input::get('gallery_id');
+        $type = Input::get('type');
+        $page = Input::get('page');
+        $perPage = config('typicms.files.per_page');
         if ($gallery_id = Input::get('gallery_id', 0)) {
             $models = $this->repository->allBy('gallery_id', $gallery_id, [], true);
+        } else if (Input::get('view') == 'filepicker') {
+            $models = $this->repository->byPageFrom($page, $perPage, $gallery_id, [], true, $type);
+            $models = $models->items;
         } else {
             $models = $this->repository->all([], true);
         }
