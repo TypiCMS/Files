5a3c4551e6e2fa10d04457152e48af2c878a64d5
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:44:23 +0100

    array validation

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -8,12 +8,11 @@ class FormRequest extends AbstractFormRequest
 {
     public function rules()
     {
-        $rules = [];
+        $rules = [
+            '*.alt_attribute' => 'max:255',
+        ];
         if ($this->hasFile('file')) {
             $rules['file'] = 'mimes:jpeg,gif,png,bmp,tiff,pdf,eps,rtf,txt,md,doc,xls,ppt,docx,xlsx,ppsx,pptx,sldx|max:2000';
-        }
-        foreach (config('translatable.locales') as $locale) {
-            $rules[$locale.'.alt_attribute'] = 'max:255';
         }
 
         return $rules;
