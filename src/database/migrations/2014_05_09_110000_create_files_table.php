e6fef7b329552f5716ec3213e6a101eca0fc9035
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:43:10 +0100

    lines

--- src/database/migrations/2014_05_09_110000_create_files_table.php
+++ src/database/migrations/2014_05_09_110000_create_files_table.php
@@ -14,11 +14,8 @@ class CreateFilesTable extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
-
             $table->integer('gallery_id')->unsigned()->nullable();
-
             $table->enum('type', ['a', 'v', 'd', 'i', 'o'])->nullable();
             $table->string('file')->nullable();
             $table->string('path')->nullable();
@@ -27,31 +24,21 @@ class CreateFilesTable extends Migration
             $table->integer('width')->unsigned()->nullable();
             $table->integer('height')->unsigned()->nullable();
             $table->integer('filesize')->unsigned()->nullable();
-
             $table->integer('position')->unsigned()->default(0);
-
             $table->timestamps();
-
             $table->foreign('gallery_id')->references('id')->on('galleries')->onDelete('cascade');
-
         });
 
         Schema::create('file_translations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('file_id')->unsigned();
-
             $table->string('locale')->index();
-
             $table->text('description');
             $table->string('alt_attribute');
-
             $table->timestamps();
-
             $table->unique(['file_id', 'locale']);
             $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade');
-
         });
     }
 
