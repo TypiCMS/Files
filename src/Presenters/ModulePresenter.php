c201fcfc2b978dc1d4e5266581ff95a6c97904df
Samuel De Backer <sdebacker@gmail.com>
2016-03-25 14:59:53 +0100

    obsolete method

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -25,30 +25,6 @@ class ModulePresenter extends Presenter
     }
 
     /**
-     * Return an icon and file name.
-     *
-     * @param int    $size  icon size
-     * @param string $field field name
-     *
-     * @return string HTML code
-     */
-    public function icon($size = 1, $field = 'document')
-    {
-        $file = $this->getPath($this->entity, $field);
-        $html = '<div class="doc">';
-        $html .= '<span class="text-center fa fa-file-o fa-'.$size.'x"></span>';
-        $html .= ' <a href="'.$file.'">';
-        $html .= $this->entity->$field;
-        $html .= '</a>';
-        if (!is_file(public_path().$file)) {
-            $html .= ' <span class="text-danger">('.trans('global.Not found').')</span>';
-        }
-        $html .= '</div>';
-
-        return $html;
-    }
-
-    /**
      * Get title.
      *
      * @return string
