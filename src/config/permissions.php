b4443723c49dedbb70b9a7415517ea2a11daaaa8
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 13:27:15 +0200

    permissions in config

---
+++ src/config/permissions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'files' => [
+        'index-files' => 'Show list',
+        'create-file' => 'Create',
+        'store-file' => 'Store',
+        'edit-file' => 'Edit',
+        'update-file' => 'Update',
+        'sort-files' => 'Sort',
+        'destroy-file' => 'Destroy',
+    ],
+];
