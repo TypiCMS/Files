818ebab0d25ba488ef7a75ead1fc3129e8aec278
Samuel De Backer <sdebacker@gmail.com>
2017-05-11 18:30:15 +0200

    fancybox

--- src/resources/views/public/_files.blade.php
+++ src/resources/views/public/_files.blade.php
@@ -3,7 +3,7 @@
     @foreach ($model->files as $file)
         <li class="files-list-item">
             @if ($file->type == 'i')
-            <a class="files-list-image fancybox" href="{!! $file->present()->thumbSrc(1200, 1200, array('resize'), 'name') !!}" data-fancybox-group="{{ $model->slug }}">
+            <a class="files-list-image" href="{!! $file->present()->thumbSrc(1200, 1200, array('resize'), 'name') !!}" data-caption="{{ $file->alt_attribute }}" data-fancybox="{{ $model->slug ? : 'group' }}">
                 <img class="files-list-image-thumb" src="{!! $file->present()->thumbSrc(370, 370, array(), 'name') !!}" alt="{{ $file->alt_attribute }}">
             </a>
             @else
