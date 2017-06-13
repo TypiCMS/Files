00dde87ce197be1d8fb2561830b0edcb84a8084a
Samuel De Backer <sdebacker@gmail.com>
2017-06-13 15:31:33 +0200

    autodiscover disabled + view composer in boot method

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -35,6 +35,11 @@ class ModuleProvider extends ServiceProvider
 
         // Observers
         File::observe(new FileObserver());
+
+        /*
+         * Sidebar view composer
+         */
+        $this->app->view->composer('core::admin._sidebar', SidebarViewComposer::class);
     }
 
     public function register()
@@ -45,11 +50,6 @@ class ModuleProvider extends ServiceProvider
          * Register route service provider
          */
         $app->register(RouteServiceProvider::class);
-
-        /*
-         * Sidebar view composer
-         */
-        $app->view->composer('core::admin._sidebar', SidebarViewComposer::class);
 
         $app->bind('Files', EloquentFile::class);
     }
