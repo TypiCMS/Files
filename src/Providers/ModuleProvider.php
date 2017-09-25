1ecc54a38dc6eed6db2f5f04e9a27ef63dc7e749
Samuel De Backer <sdebacker@gmail.com>
2017-09-25 14:59:52 +0200

    More explicit tag for publishing

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -30,7 +30,7 @@ class ModuleProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/vendor/files'),
-        ], 'views');
+        ], 'typicms-views');
 
         AliasLoader::getInstance()->alias('Files', Files::class);
 
