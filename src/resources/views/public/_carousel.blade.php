c6c2c1255eb70eb979b9a1f140b87b783ad87d37
Samuel De Backer <sdebacker@gmail.com>
2018-09-10 16:14:37 +0200

    _carousel template

--- src/resources/views/public/_carousel.blade.php
+++ src/resources/views/public/_carousel.blade.php
@@ -1,14 +1,14 @@
 @if ($images->count() > 0)
-<div class="news-images-container">
-    <div class="news-images @if($images->count() > 1)news-images-swiper @endif container-slides swiper-container">
-        <div class="slides-list swiper-wrapper">
+<div class="carousel-container">
+    <div class="carousel @if($images->count() > 1)carousel-swiper @endif swiper-container">
+        <div class="swiper-wrapper">
             @foreach ($images as $image)
-            <img class="news-image swiper-slide" src="{!! $image->present()->thumbSrc(2880, 1920, [], 'name') !!}" alt="" width="1440" height="960">
+            <img class="carousel-image swiper-slide" src="{!! $image->present()->thumbSrc(2880, 1920, [], 'name') !!}" alt="">
             @endforeach
         </div>
     </div>
     @if($images->count() > 1)
-    <div class="swiper-pagination limited-width"></div>
+    <div class="swiper-pagination"></div>
     @endif
 </div>
 @endif
