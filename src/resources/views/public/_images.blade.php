fa5430c4dfd4862aef718929e488c4155a66c205
Samuel De Backer <sdebacker@gmail.com>
2018-11-06 17:25:12 +0100

    old array syntax removed + argument removed from thumbSrc()

--- src/resources/views/public/_images.blade.php
+++ src/resources/views/public/_images.blade.php
@@ -2,8 +2,8 @@
     <ul class="images-list">
     @foreach ($model->images as $image)
         <li class="images-list-item">
-            <a class="images-list-image" href="{!! $image->present()->thumbSrc(1200, 1200, array('resize'), 'name') !!}" data-caption="{{ $image->alt_attribute }}" data-fancybox="{{ $model->slug ? : 'group' }}">
-                <img class="images-list-image-thumb" src="{!! $image->present()->thumbSrc(370, 370, array(), 'name') !!}" alt="{{ $image->alt_attribute }}">
+            <a class="images-list-image" href="{!! $image->present()->thumbSrc(1200, 1200, ['resize']) !!}" data-caption="{{ $image->alt_attribute }}" data-fancybox="{{ $model->slug ? : 'group' }}">
+                <img class="images-list-image-thumb" src="{!! $image->present()->thumbSrc(370, 370) !!}" alt="{{ $image->alt_attribute }}">
             </a>
         </li>
     @endforeach
