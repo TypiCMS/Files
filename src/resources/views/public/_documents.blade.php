b4fe975791be89468a67d42bf2020d526067631d
Samuel De Backer <sdebacker@gmail.com>
2019-05-08 09:36:50 +0200

    Storage::url

--- src/resources/views/public/_documents.blade.php
+++ src/resources/views/public/_documents.blade.php
@@ -2,7 +2,7 @@
     <ul class="document-list-list">
     @foreach ($model->documents as $document)
         <li class="document-list-item">
-            <a class="document-list-item-link" href="{{ asset('storage/'.$document->path) }}" download>
+            <a class="document-list-item-link" href="{{ Storage::url($document->path) }}" download>
                 <span class="document-list-item-icon fa fa-file-o fa-2x"></span>
                 <span class="document-list-item-filename">{{ $document->name }}</span> <small class="files-list-document-filesize">{{ $document->present()->filesize }}</small>
             </a>
