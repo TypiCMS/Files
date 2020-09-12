e80a62b1cc4f62d94658a65cee1ec4b1ce788ba0
Samuel De Backer <sdebacker@gmail.com>
2020-09-12 15:56:50 +0200

    $rules removed

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -8,13 +8,11 @@ class FormRequest extends AbstractFormRequest
 {
     public function rules()
     {
-        $rules = [
+        return [
             'folder_id' => 'nullable|integer',
             'alt_attribute.*' => 'max:255',
             'description.*' => 'max:255',
             'name' => 'mimes:jpeg,gif,png,bmp,tiff,pdf,eps,rtf,txt,md,doc,xls,ppt,docx,xlsx,ppsx,pptx,sldx|max:'.config('typicms.max_file_upload_size', 2000),
         ];
-
-        return $rules;
     }
 }
