b0c0289150068d8de07776c80be2e2b8ab39f6f1
Samuel De Backer <sdebacker@gmail.com>
2020-09-18 11:54:09 +0200

    FileObserver moved to Files module

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -4,11 +4,11 @@ namespace TypiCMS\Modules\Files\Providers;
 
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
-use TypiCMS\Modules\Core\Observers\FileObserver;
 use TypiCMS\Modules\Core\Services\FileUploader;
 use TypiCMS\Modules\Files\Composers\SidebarViewComposer;
 use TypiCMS\Modules\Files\Facades\Files;
 use TypiCMS\Modules\Files\Models\File;
+use TypiCMS\Modules\Files\Observers\FileObserver;
 
 class ModuleProvider extends ServiceProvider
 {
