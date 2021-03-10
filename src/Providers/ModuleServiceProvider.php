440f1b853f2906e7329d567af05c6f9f39a0cedd
Samuel De Backer <sdebacker@gmail.com>
2021-03-10 15:45:40 +0100

    files moved

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -20,15 +20,15 @@ class ModuleServiceProvider extends ServiceProvider
         $modules = $this->app['config']['typicms']['modules'];
         $this->app['config']->set('typicms.modules', array_merge(['files' => []], $modules));
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views/', 'files');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'files');
 
         $this->publishes([
-            __DIR__.'/../database/migrations/create_files_table.php.stub' => getMigrationFileName('create_files_table'),
-            __DIR__.'/../database/migrations/create_model_has_files_table.php.stub' => getMigrationFileName('create_model_has_files_table'),
+            __DIR__.'/../../database/migrations/create_files_table.php.stub' => getMigrationFileName('create_files_table'),
+            __DIR__.'/../../database/migrations/create_model_has_files_table.php.stub' => getMigrationFileName('create_model_has_files_table'),
         ], 'migrations');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/files'),
+            __DIR__.'/../../resources/views' => resource_path('views/vendor/files'),
         ], 'views');
 
         AliasLoader::getInstance()->alias('Files', Files::class);
