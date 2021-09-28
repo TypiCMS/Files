e8c31c59062c1b229ece4050c1d47d182e4f3209
Samuel De Backer <sdebacker@gmail.com>
2021-09-28 10:55:46 +0200

    Update _carousel.blade.php

--- resources/views/public/_carousel.blade.php
+++ resources/views/public/_carousel.blade.php
@@ -6,9 +6,43 @@
             <img class="carousel-image swiper-slide" src="{!! $image->present()->image(2880, 1920) !!}" alt="">
             @endforeach
         </div>
+        <div class="carousel-button carousel-button-prev swiper-button-prev swiper-button-white"></div>
+        <div class="carousel-button carousel-button-next swiper-button-next swiper-button-white"></div>
     </div>
     @if ($model->images->count() > 1)
-    <div class="swiper-pagination"></div>
+    <div class="carousel-pagination swiper-pagination"></div>
     @endif
 </div>
 @endif
+
+@push('js')
+<script>
+    new Swiper('.carousel', {
+        loop: true,
+        grabCursor: true,
+        speed: 800,
+        autoplay: {
+            delay: 6000,
+        },
+        navigation: {
+            prevEl: '.carousel-button-prev',
+            nextEl: '.carousel-button-next',
+        },
+        pagination: {
+            el: '.carousel-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        // slidesPerView: 1,
+        // allowTouchMove: true,
+        // watchOverflow: true,
+        // spaceBetween: 50,
+        // breakpoints: {
+        //   1100: {
+        //     slidesPerView: 2,
+        //   },
+        // },
+    });
+
+</script>
+@endpush
